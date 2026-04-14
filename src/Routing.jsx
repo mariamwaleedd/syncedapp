7baca3087a6622a8ee8139848c171039c6cd6735
Mariam Waleed <mariamwaleed2005@gmail.com>
2026-04-14 14:34:25 +0200

    feat: signup page / updated linking

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 
 const Routing = () => {
   return (
@@ -9,6 +10,8 @@ const Routing = () => {
       <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/home" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+
     </Routes>
   );
 };
